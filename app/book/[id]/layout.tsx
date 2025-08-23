3be177a93d77171fb766cdabf7d33f960c1f651a
Abddev09 <abddev09@gmail.com>
2025-08-23 11:11:54 +0500

    params

--- app/book/[id]/layout.tsx
+++ app/book/[id]/layout.tsx
@@ -1,18 +1,6 @@
-import { getAllBooks } from "@/lib/api"
 import { ReactNode } from "react"
 
-export async function generateStaticParams() {
-  const response = await getAllBooks()
-  const books = response.data || []
 
-  if (books.length === 0) {
-    return [{ id: "placeholder" }] // yoki hatto []
-  }
-
-  return books.map(book => ({
-    id: book.id.toString(),
-  }))
-}
 
 
 export default function BookLayout({ children }: { children: ReactNode }) {
